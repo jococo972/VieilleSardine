2bbeef04be6fa9a35aeb6d876dca80c825abaabc
Anes <anes.mahi@gmail.com>
2014-04-10 15:26:12 +0200

    mise à jour Entité client - Préparer la configuration FOS

--- src/VieilleSardine/UserBundle/Entity/Client.php
+++ src/VieilleSardine/UserBundle/Entity/Client.php
@@ -12,15 +12,98 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  */
 class Client extends BaseUser{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_client", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idClient;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="civilite", type="string", length=5, nullable=false)
+     */
+    private $civilite;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=20, nullable=false)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=20, nullable=false)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tel_principal", type="string", length=15, nullable=false)
+     */
+    private $telPrincipal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="tel_secondaire", type="string", length=15, nullable=false)
+     */
+    private $telSecondaire;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sexe", type="string", length=1, nullable=false)
+     */
+    private $sexe;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero_voie", type="string", length=5, nullable=false)
+     */
+    private $numeroVoie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type_voie", type="string", length=15, nullable=false)
+     */
+    private $typeVoie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code_postal", type="string", length=8, nullable=false)
+     */
+    private $codePostal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=15, nullable=false)
+     */
+    private $ville;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pays", type="string", length=15, nullable=false)
+     */
+    private $pays;
 
     /**
      * @var integer
      *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(name="client_a_livrer", type="integer", nullable=false)
      */
-    protected $id;
+    private $clientALivrer;
 
     /**
      * @var \DateTime
@@ -29,13 +112,296 @@ class Client extends BaseUser{
      */
     private $dateCreate;
 
+    public function __construct() {
+        parent::__construct();
+        //OUR OWN LOGIC 
+    }
+    
+    
     /**
-     * Get id
+     * Get idClient
      *
      * @return integer 
      */
-    public function getId() {
-        return $this->id;
+    public function getIdClient()
+    {
+        return $this->idClient;
+    }
+
+    /**
+     * Set civilite
+     *
+     * @param string $civilite
+     * @return Client
+     */
+    public function setCivilite($civilite)
+    {
+        $this->civilite = $civilite;
+    
+        return $this;
+    }
+
+    /**
+     * Get civilite
+     *
+     * @return string 
+     */
+    public function getCivilite()
+    {
+        return $this->civilite;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return Client
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return Client
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set telPrincipal
+     *
+     * @param string $telPrincipal
+     * @return Client
+     */
+    public function setTelPrincipal($telPrincipal)
+    {
+        $this->telPrincipal = $telPrincipal;
+    
+        return $this;
+    }
+
+    /**
+     * Get telPrincipal
+     *
+     * @return string 
+     */
+    public function getTelPrincipal()
+    {
+        return $this->telPrincipal;
+    }
+
+    /**
+     * Set telSecondaire
+     *
+     * @param string $telSecondaire
+     * @return Client
+     */
+    public function setTelSecondaire($telSecondaire)
+    {
+        $this->telSecondaire = $telSecondaire;
+    
+        return $this;
+    }
+
+    /**
+     * Get telSecondaire
+     *
+     * @return string 
+     */
+    public function getTelSecondaire()
+    {
+        return $this->telSecondaire;
+    }
+
+    /**
+     * Set sexe
+     *
+     * @param string $sexe
+     * @return Client
+     */
+    public function setSexe($sexe)
+    {
+        $this->sexe = $sexe;
+    
+        return $this;
+    }
+
+    /**
+     * Get sexe
+     *
+     * @return string 
+     */
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+
+    /**
+     * Set numeroVoie
+     *
+     * @param string $numeroVoie
+     * @return Client
+     */
+    public function setNumeroVoie($numeroVoie)
+    {
+        $this->numeroVoie = $numeroVoie;
+    
+        return $this;
+    }
+
+    /**
+     * Get numeroVoie
+     *
+     * @return string 
+     */
+    public function getNumeroVoie()
+    {
+        return $this->numeroVoie;
+    }
+
+    /**
+     * Set typeVoie
+     *
+     * @param string $typeVoie
+     * @return Client
+     */
+    public function setTypeVoie($typeVoie)
+    {
+        $this->typeVoie = $typeVoie;
+    
+        return $this;
+    }
+
+    /**
+     * Get typeVoie
+     *
+     * @return string 
+     */
+    public function getTypeVoie()
+    {
+        return $this->typeVoie;
+    }
+
+    /**
+     * Set codePostal
+     *
+     * @param string $codePostal
+     * @return Client
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+    
+        return $this;
+    }
+
+    /**
+     * Get codePostal
+     *
+     * @return string 
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     * @return Client
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string 
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Set pays
+     *
+     * @param string $pays
+     * @return Client
+     */
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
+    
+        return $this;
+    }
+
+    /**
+     * Get pays
+     *
+     * @return string 
+     */
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    /**
+     * Set clientALivrer
+     *
+     * @param integer $clientALivrer
+     * @return Client
+     */
+    public function setClientALivrer($clientALivrer)
+    {
+        $this->clientALivrer = $clientALivrer;
+    
+        return $this;
+    }
+
+    /**
+     * Get clientALivrer
+     *
+     * @return integer 
+     */
+    public function getClientALivrer()
+    {
+        return $this->clientALivrer;
     }
 
     /**
@@ -44,9 +410,10 @@ class Client extends BaseUser{
      * @param \DateTime $dateCreate
      * @return Client
      */
-    public function setDateCreate($dateCreate) {
+    public function setDateCreate($dateCreate)
+    {
         $this->dateCreate = $dateCreate;
-
+    
         return $this;
     }
 
@@ -55,13 +422,301 @@ class Client extends BaseUser{
      *
      * @return \DateTime 
      */
-    public function getDateCreate() {
+    public function getDateCreate()
+    {
         return $this->dateCreate;
     }
 
-    public function __construct() {
-        parent::__construct();
-        $this->dateCreate = new \Datetime();
+  
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
     }
 
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return Client
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set usernameCanonical
+     *
+     * @param string $usernameCanonical
+     * @return Client
+     */
+    public function setUsernameCanonical($usernameCanonical)
+    {
+        $this->usernameCanonical = $usernameCanonical;
+    
+        return $this;
+    }
+
+    /**
+     * Get usernameCanonical
+     *
+     * @return string 
+     */
+    public function getUsernameCanonical()
+    {
+        return $this->usernameCanonical;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Client
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set emailCanonical
+     *
+     * @param string $emailCanonical
+     * @return Client
+     */
+    public function setEmailCanonical($emailCanonical)
+    {
+        $this->emailCanonical = $emailCanonical;
+    
+        return $this;
+    }
+
+    /**
+     * Get emailCanonical
+     *
+     * @return string 
+     */
+    public function getEmailCanonical()
+    {
+        return $this->emailCanonical;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return Client
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return Client
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Get lastLogin
+     *
+     * @return \DateTime 
+     */
+    public function getLastLogin()
+    {
+        return $this->lastLogin;
+    }
+
+    /**
+     * Set locked
+     *
+     * @param boolean $locked
+     * @return Client
+     */
+    public function setLocked($locked)
+    {
+        $this->locked = $locked;
+    
+        return $this;
+    }
+
+    /**
+     * Get locked
+     *
+     * @return boolean 
+     */
+    public function getLocked()
+    {
+        return $this->locked;
+    }
+
+    /**
+     * Set expired
+     *
+     * @param boolean $expired
+     * @return Client
+     */
+    public function setExpired($expired)
+    {
+        $this->expired = $expired;
+    
+        return $this;
+    }
+
+    /**
+     * Get expired
+     *
+     * @return boolean 
+     */
+    public function getExpired()
+    {
+        return $this->expired;
+    }
+
+    /**
+     * Get expiresAt
+     *
+     * @return \DateTime 
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * Set confirmationToken
+     *
+     * @param string $confirmationToken
+     * @return Client
+     */
+    public function setConfirmationToken($confirmationToken)
+    {
+        $this->confirmationToken = $confirmationToken;
+    
+        return $this;
+    }
+
+    /**
+     * Get confirmationToken
+     *
+     * @return string 
+     */
+    public function getConfirmationToken()
+    {
+        return $this->confirmationToken;
+    }
+
+    /**
+     * Get passwordRequestedAt
+     *
+     * @return \DateTime 
+     */
+    public function getPasswordRequestedAt()
+    {
+        return $this->passwordRequestedAt;
+    }
+
+
+    /**
+     * Get roles
+     *
+     * @return array 
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * Set credentialsExpired
+     *
+     * @param boolean $credentialsExpired
+     * @return Client
+     */
+    public function setCredentialsExpired($credentialsExpired)
+    {
+        $this->credentialsExpired = $credentialsExpired;
+    
+        return $this;
+    }
+
+    /**
+     * Get credentialsExpired
+     *
+     * @return boolean 
+     */
+    public function getCredentialsExpired()
+    {
+        return $this->credentialsExpired;
+    }
+
+
+    /**
+     * Get credentialsExpireAt
+     *
+     * @return \DateTime 
+     */
+    public function getCredentialsExpireAt()
+    {
+        return $this->credentialsExpireAt;
+    }
 }
