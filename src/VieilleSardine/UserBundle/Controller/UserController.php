693d0e363aedab0a479239b90f76ff246c1004be
Anes <anes.mahi@gmail.com>
2014-04-21 16:34:15 +0200

    COrrection commit du 21/04

--- src/VieilleSardine/UserBundle/Controller/UserController.php
+++ src/VieilleSardine/UserBundle/Controller/UserController.php
@@ -7,35 +7,4 @@ use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller {
 
-    public function inscriptionAction(Request $request) {
-
-        $defaults = array(
-            'DateDeNaissance' => new \DateTime('tomorrow'),
-        );
-
-        $form = $this->createFormBuilder($defaults)
-                ->add('Nom', 'text')
-                ->add('Prenom', 'text')
-                ->add('DateDeNaissance', 'birthday')
-                ->getForm();
-
-        $form->handleRequest($request);
-        
-        if ($form->isValid()) {
-            $data = $form->getData();
-            var_dump($data);
-
-            // ... exécutez une action, comme enregistrer des
-            // données dans une base de données
-
-//            return $this->redirect($this->generateUrl('vieille_sardine_commande_homepage', array('data' => $data)));
-            return $this->render('VieilleSardineCommandeBundle:Commande:index.html.twig', array(
-                    'data' => $data,
-        ));
-        }
-        return $this->render('VieilleSardineUserBundle:Inscription:inscription.html.twig', array(
-                    'form' => $form->createView(),
-        ));
-    }
-
 }
