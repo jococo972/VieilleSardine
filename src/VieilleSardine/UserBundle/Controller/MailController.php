d987c9614e9f5a78a533950f3c61b1f0a00fb31c
Anes <anes.mahi@gmail.com>
2014-04-21 16:47:29 +0200

    Correction commit du 21/04

--- src/VieilleSardine/UserBundle/Controller/MailController.php
+++ src/VieilleSardine/UserBundle/Controller/MailController.php
@@ -6,15 +6,4 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MailController extends Controller {
 
-    public function sendMailAction($name) {
-        $message = \Swift_Message::newInstance()
-                ->setSubject('Hello Email')
-                ->setFrom('anes.mahi@gmail.com')
-                ->setTo('m.anes@hotmail.fr')
-                ->setBody($this->renderView('VieilleSardineUserBundle:Mail:mailBody.html.twig', array('name' => $name)));
-        $this->get('mailer')->send($message);
-
-        return $this->render('VieilleSardineUserBundle:Mail:sendConfirmation.html.twig');
-    }
-
 }
