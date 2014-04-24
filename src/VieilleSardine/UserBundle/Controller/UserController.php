98185e296a5f2889b5e7d308faea4519263e9c86
Anes <anes.mahi@gmail.com>
2014-04-24 20:48:37 +0200

    Mise à jour entité Client

--- src/VieilleSardine/UserBundle/Controller/UserController.php
+++ src/VieilleSardine/UserBundle/Controller/UserController.php
@@ -3,7 +3,6 @@
 namespace VieilleSardine\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller {
 
